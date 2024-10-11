b21870e54292258ba0aa33f1df6c5105374b6561
Harshad Deshmukh <deshmukhharshad@ymail.com>
2024-10-11 15:02:03 +0530

    updated title to MLOPS

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
 
-    <h1>Gen AI App for ALL BUSSINESSES</h1>
+    <h1>Gen AI App for MLOPS</h1>
 	<h2>Upload your data to get Gen Ai predictions</h2>
     <img
       alt="Vue logo"
